60751577a5e229b1edf65200e36b2c6cd1f0f431
Kevin Marrec <kevin@marrec.io>
2022-01-14 15:44:11 +0100

    chore(app): add dio error interceptor

--- app/lib/shared/dio.dart
+++ app/lib/shared/dio.dart
@@ -3,10 +3,25 @@ import 'package:riverpod/riverpod.dart';
 
 export 'package:dio/dio.dart' show DioError;
 
-final dioProvider = Provider(
-  (ref) => Dio(
+final dioProvider = Provider((ref) {
+  final dio = Dio(
     BaseOptions(
       baseUrl: 'http://localhost:4000',
     ),
-  ),
-);
+  );
+
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onError: (DioError e, handler) {
+        if (e.type == DioErrorType.other) {
+          // TODO
+          return;
+        }
+
+        return handler.next(e);
+      },
+    ),
+  );
+
+  return dio;
+});
